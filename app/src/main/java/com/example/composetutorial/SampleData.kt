f8c48bf747a60ff1bf5a0fa67f9684f16611c466
Aino <aino.mp@hotmail.com>
2024-01-29 22:40:22 +0200

    Homework 1 and Homework 2 code

---
+++ app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,63 @@
+package com.example.composetutorial
+
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Chun-Li",
+            "一度身につけた技って 簡単には忘れられないものなの"
+        ),
+        Message(
+            "Chun-Li",
+            """I stood when standing was not easy.""".trim()
+        ),
+        Message(
+            "Chun-Li",
+            """The secret to strength is daily training.""".trim()
+        ),
+        Message(
+            "Chun-Li",
+            "You can't show off if you don't have any skills!"
+        ),
+        Message(
+            "Chun-Li",
+            """My Kung-Fu can't be defeated by a mere imitator!""".trim()
+        ),
+        Message(
+            "Chun-Li",
+            "I don't think putting \"Real Street Fighter\" on your resume is going to land you many interviews."
+        ),
+        Message(
+            "Chun-Li",
+            "I fight for peace, but war is all I ever see!\""
+        ),
+        Message(
+            "Chun-Li",
+            "Let this be the last battle... I must destroy evil!"
+        ),
+        Message(
+            "Chun-Li",
+            "The history of Chinese martial arts shows that power and accuracy are borne through training, and every kick I land is proof this."
+        ),
+        Message(
+            "Chun-Li",
+            "How many times do you need me to spell it out for you. I-am-the-real-Chun-Li!"
+        ),
+        Message(
+            "Chun-Li",
+            "You're under arrest! I'm taking you to jail - or an animal shelter!"
+        ),
+        Message(
+            "Chun-Li",
+            "Often imitated but never perfected!"
+        ),
+        Message(
+            "Chun-Li",
+            "You clearly don't have what it takes to stand up to my kicks!"
+        ),
+    )
+}
